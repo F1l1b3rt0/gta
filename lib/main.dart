3f84c8fe26ebc6395e2b74676458bbae86a5e4af
migueliocss <josemiguelhernandezresendiz8@gmail.com>
2026-03-23 20:43:58 -0600

    subí los archivos que me pediste aleman
    Para que veas elfu9ncionamiento de los botnes

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gta/login_admin.dart';
-void main(){
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/menu_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'AlemanApp',
       debugShowCheckedModeBanner: false,
-      title: 'Gta',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
-        useMaterial3: true,
-      ),
-      home: const Dashboard(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/menu': (context) => MenuScreen(),
+      },
     );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  const Dashboard({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Bienvenido a Gta')),
-      body: Center(
-        child: Padding(padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.business_center, size: 80, color: Colors.blueAccent),
-            const SizedBox(height: 20),
-            const Text('Gestor de Turnos Afolicos',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> const LoginAdmin(),
-              ),
-              );
-            }, 
-            child: const Text('Ingresar'),
-            ),
-          ],
-        ),
-        ),
-        ),
-      );
   }
 }
